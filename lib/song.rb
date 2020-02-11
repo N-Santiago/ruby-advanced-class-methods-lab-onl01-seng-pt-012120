e38210a0e8a40f4c918aa55b0c73a798696bd8d0
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-11 20:20:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,7 +55,7 @@ class Song
   end
   
     def self.create_from_filename(filename)
-      result = new_from_filename.split(" - ")
+      result = new_from_filename(filename)
       song = self.create 
       song.name = result.name 
       song.artist_name = result.artist_name
