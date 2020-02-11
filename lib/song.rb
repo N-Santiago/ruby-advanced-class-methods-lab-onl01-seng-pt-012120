8bb8752794721f5252e421c3588192b4878a33de
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-11 19:56:44 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,6 +46,11 @@ class Song
     end 
     
     def self.new_from_filename(filename)
-      song_array = filename.
-    
+      song_array = filename.split(" - ")
+      song_array[1] = song_array[1].chomp(".mp3") #chomp is used to remove the
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
 end
