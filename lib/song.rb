b91152a455c0fde0fd8fb841679af696fcae35b7
Norberto Santiago <norbertosantiago82@gmail.com>
2020-02-11 19:33:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
       end
     end
     
-    def self.alphabetical(name = song.name)
+    def self.alphabetical(name)
       self.all.sort_by {|song| song <=> name}
       song 
     end 
